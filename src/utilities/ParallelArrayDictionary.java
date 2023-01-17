e1444e1cc5e619c5d64374bff97d5d946e10bfcf
ismakh9 <122399506+ismakh9@users.noreply.github.com>
2023-01-17 14:44:05 -0500

    new update
    test

--- src/utilities/ParallelArrayDictionary.java
+++ src/utilities/ParallelArrayDictionary.java
@@ -2,6 +2,7 @@ package utilities;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
@@ -48,7 +49,9 @@ public class ParallelArrayDictionary<Key, Value> implements Map<Key,Value>
 
 	@Override
 	public void putAll(Map<? extends Key, ? extends Value> m) {
+		//loop through the map to get key and value saperately
 		for(Entry<? extends Key, ? extends Value> entry : m.entrySet()) {
+			//put keys and values to their arraysets
 			put(entry.getKey(), entry.getValue());
 		}
 	}
@@ -62,20 +65,39 @@ public class ParallelArrayDictionary<Key, Value> implements Map<Key,Value>
 
 	@Override
 	public Set<Key> keySet() {
-		// TODO Auto-generated method stub
-		return null;
+		//new Arrayset to store the keys
+		ArraySet<Key> newKeys = new ArraySet<Key>();
+		//loop through the keys
+		for(int i = 0; i< _keys.size()-1; i++) {
+			//add the keys to the new arrayset newKeys
+			newKeys.add(_keys.get(i));
+		}
+		return newKeys;
 	}
 
 	@Override
 	public Collection<Value> values() {
-		// TODO Auto-generated method stub
-		return null;
+		//new Arrayset to store the values
+		ArraySet<Value> newValue = new ArraySet<Value>();
+		//loop through the values 
+		for(int i = 0; i< _values.size()-1; i++) {
+			//add the values to the new arrayset newValue
+			newValue.add(_values.get(i));
+		}
+		return newValue;
 	}
 
 	@Override
 	public Set<Entry<Key, Value>> entrySet() {
-		// TODO Auto-generated method stub
-		return null;
+		//create java hash_map set to store the keys and values
+		HashMap<Key, Value> hash_map = new HashMap<Key, Value>();
+		//loop through the set to get index of the arrayset
+		for(int i = 0; i< _values.size()-1; i++) {
+			//store both the keys and the value to the hash_map
+			hash_map.put(_keys.get(i), _values.get(i));
+		}
+		//return the entrySet of hash_map
+		return hash_map.entrySet();
 	}
 
 }
